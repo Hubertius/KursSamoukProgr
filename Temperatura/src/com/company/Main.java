f12a845853aaabaef8869ed87c46cc794a7bd66b
Hubertius <hubert.kopec@interia.pl>
2021-11-29 19:39:22 +0100

    Temperature

---
+++ Temperatura/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        temperature(Integer.valueOf(input));
+    }
+
+    public static void temperature(int temperature) {
+        if(temperature < 0) {
+            System.out.println("Temperature is BELOW 0");
+        } else if(temperature == 0) {
+            System.out.println("Temperature is EQUAL to 0");
+        } else {
+            System.out.println("Temperature is HIGHER than 0");
+        }
+    }
+}
